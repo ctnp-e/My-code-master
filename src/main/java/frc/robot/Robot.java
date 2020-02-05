269a9f813445120e57ce81f0650eb908a9339ae9
ctnp <catnipiscatweed@gmail.com>
2020-02-04 18:24:09 -0800

    green!=green,yellow!=yellow

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -161,7 +161,7 @@ public class Robot extends TimedRobot {
         colorString = colors[color_number];
         rec_color=1;
       } else if (match.color == kGreenTarget) {
-        if(rec_color == 0){
+        if(rec_color == 3){
           color_number=1;
           rec_color=0;
         }else{
@@ -169,7 +169,7 @@ public class Robot extends TimedRobot {
         }
         colorString = colors[color_number];
       } else if (match.color == kYellowTarget) {
-        if(rec_color == 2){
+        if(rec_color == 1){
           color_number = 3;
           rec_color=2;
         }else{
